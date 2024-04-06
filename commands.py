bd2ab0b395d3be51d8ecb960e52a3d9a1bd298e7
Juli Storino <julieta_storino@mi.unc.edu.ar>
2024-04-06 14:12:46 -0300

    implementación del get_file_listing

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 import socket
 from constants import *
-
+import os
+import os.path
 
 def parse_and_run(self, line):
     """
@@ -12,7 +13,20 @@ def get_file_listing(self):
     """
     Obtiene la lista de archivos disponibles en el servidor.
     """
-    pass
+    if not os.path.exists(self.directory): # FALTA: Ver que pasa si el directorio no existe
+        # Si el directorio no existe envía un mensaje de error
+        mensaje = f'{FILE_NOT_FOUND} {error_messages[FILE_NOT_FOUND]}\r\n'.encode('ascii')
+        self.socket.send(mensaje)
+        return
+    # Lista los archivos del directorio
+    files = os.listdir(self.directory)
+    # Redacta la confirmación de lectura
+    mensaje = f'{CODE_OK} {error_messages[CODE_OK]}\r\n'.encode('ascii')
+    # Redacta la lista de archivos
+    for file in files:
+        mensaje += file.encode('ascii') + b'\r\n'
+    mensaje += b'\r\n'
+    self.socket.send(mensaje)
 
 def get_metadata(self, FILENAME):
     """
